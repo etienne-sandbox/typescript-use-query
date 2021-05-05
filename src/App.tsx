2326b35f34b6d076a039c81c0f7e7cd0d6df80ae
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-05 12:01:39 +0200

    Get workouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Ky from "ky";
+
+interface Workout {
+  date: string;
+  distance: number;
+  duration: number;
+  id: string;
+  place: string;
+  placeName: string;
+  speed: number;
+  user: string;
+  username: string;
+}
+
+interface Data {
+  results: Array<Workout>;
+  total: number;
+}
 
 export function App() {
-  return <div className="App">Hello React</div>;
+  const [data, setData] = useState<Data | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const json = await Ky.get("http://localhost:3001/workouts").json<Data>();
+      console.log(json);
+      setData(json);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div className="App">
+      {data === null ? (
+        <p>No data</p>
+      ) : (
+        data.results.map((workout) => {
+          return <div key={workout.id}>{workout.speed.toFixed(2)} km/h</div>;
+        })
+      )}
+    </div>
+  );
 }
