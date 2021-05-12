1eb1cec600d7ee50392901ddab53e310feec8f59
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-12 10:38:13 +0200

    Add places

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,20 @@ const WorkoutsResultsSchema = z.object({
 
 type WorkoutsResults = z.infer<typeof WorkoutsResultsSchema>;
 
+const PlaceSchema = z.object({
+  image: z.string(),
+  name: z.string(),
+  slug: z.string(),
+  workoutCount: z.number(),
+});
+
+const PlacesResultSchema = z.object({
+  total: z.number(),
+  results: z.array(PlaceSchema),
+});
+
+type PlacesResult = z.infer<typeof PlacesResultSchema>;
+
 export function App() {
   const [workouts, setWorkouts] = useState<WorkoutsResults | null>(null);
 
@@ -41,19 +55,49 @@ export function App() {
     };
   }, []);
 
+  const [places, setPlaces] = useState<PlacesResult | null>(null);
+
+  useEffect(() => {
+    let cancelled = false;
+    Ky.get("http://localhost:3001/places")
+      .json()
+      .then((data) => {
+        if (cancelled) {
+          return;
+        }
+        const result = PlacesResultSchema.parse(data);
+        setPlaces(result);
+      });
+
+    return () => {
+      cancelled = true;
+    };
+  }, []);
+
   return (
     <div className="App">
-      {workouts === null ? (
-        <p>Loading...</p>
-      ) : (
-        workouts.results.map((workout) => {
-          return (
-            <div key={workout.id}>
-              {workout.userName} - {workout.distance}m - {workout.duration}min
-            </div>
-          );
-        })
-      )}
+      <div>
+        {workouts === null ? (
+          <p>Loading...</p>
+        ) : (
+          workouts.results.map((workout) => {
+            return (
+              <div key={workout.id}>
+                {workout.userName} - {workout.distance}m - {workout.duration}min
+              </div>
+            );
+          })
+        )}
+      </div>
+      <div>
+        {places === null ? (
+          <p>Loading...</p>
+        ) : (
+          places.results.map((place) => {
+            return <div key={place.slug}>{place.name}</div>;
+          })
+        )}
+      </div>
     </div>
   );
 }
