1bc413c467ebf63403e6100d68ec1e51fb11acc3
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-18 16:56:17 +0200

    Add Resource type

--- src/useQuery.ts
+++ src/useQuery.ts
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import * as z from "zod";
 
+type Resource<T> =
+  | { status: "void" }
+  | { status: "pending" }
+  | { status: "resolved"; data: T }
+  | { status: "rejected"; error: unknown };
+
 export function useQuery<T>(url: string, schema: z.Schema<T>): T | null {
   const [data, setData] = useState<T | null>(null);
 
