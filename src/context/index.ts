4f0b4d7026235f278758503f1196f2bfd111eaa8
Etienne Dldc <e.deladonchamps@gmail.com>
2021-05-19 16:19:00 +0200

    Implement context

--- src/context/index.ts
+++ src/context/index.ts
@@ -5,10 +5,12 @@ const store = createStore();
 const NumContext = createContext(42);
 
 const num1 = store.get(NumContext); // 42
+console.log({ num1 });
 
 store.set(NumContext, 7);
 
-const num = store.get(NumContext); // 7
+const num2 = store.get(NumContext); // 7
+console.log({ num2 });
 
 if (store.has(NumContext)) {
   console.log(`Store has a NumContext`);
@@ -18,3 +20,24 @@ if (store.has(NumContext)) {
 store.set(NumContext, 7444);
 
 store.remove(NumContext);
+
+const StrContext = createContext("yolo");
+
+const str = store.get(StrContext);
+console.log({ str });
+
+store.set(StrContext, "Youpi");
+
+const str2 = store.get(StrContext);
+console.log({ str2 });
+
+type User = {
+  age: number;
+  name: string;
+};
+
+const UserContext = createContext<User | null>(null);
+
+store.set(UserContext, { name: "Etienne", age: 25 });
+const user = store.get(UserContext);
+console.log({ user });
--- src/context/lib.ts
+++ src/context/lib.ts
@@ -1,1 +1,45 @@
-export {};
+const CONTEXT = Symbol.for(
+  "__INTERNAL_CONTEXT_DO_NOT_USE_OR_YOU_WILL_GET_FIRED"
+);
+
+interface Store {
+  get: <T>(context: Context<T>) => T;
+  set: <T>(context: Context<T>, value: T) => void;
+  has: (context: Context<unknown>) => boolean;
+  remove: (context: Context<unknown>) => void;
+}
+
+interface Context<T> {
+  [CONTEXT]: {
+    defaultValue: T;
+  };
+}
+
+export function createStore(): Store {
+  const data = new Map<Context<unknown>, unknown>();
+  return {
+    get<T>(context: Context<T>): T {
+      if (data.has(context)) {
+        return data.get(context) as any;
+      }
+      return context[CONTEXT].defaultValue;
+    },
+    set<T>(context: Context<T>, value: T): void {
+      data.set(context, value);
+    },
+    has(context: Context<unknown>): boolean {
+      return data.has(context);
+    },
+    remove(context: Context<unknown>): void {
+      data.delete(context);
+    },
+  };
+}
+
+export function createContext<T>(defaultValue: T): Context<T> {
+  return {
+    [CONTEXT]: {
+      defaultValue,
+    },
+  };
+}
