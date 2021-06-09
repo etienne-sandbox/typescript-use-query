b0290380c180f1b71d5b1f5dc8672a2144116b79
Etienne Dldc <e.deladonchamps@gmail.com>
2021-06-09 12:11:47 +0200

    Fetch workouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+interface Workout {
+  id: string;
+  date: string;
+  place: string;
+  distance: number;
+  duration: number;
+  user: string;
+  placeName: string;
+  speed: number;
+  userName: string;
+}
+
+interface WorkoutsResult {
+  results: Array<Workout>;
+  total: number;
+}
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [workouts, setWorkouts] = useState<WorkoutsResult | null>(null);
+
+  useEffect(() => {
+    let canceled = false;
+    fetch("http://localhost:3001/workouts").then(async (response) => {
+      const data = await response.json();
+      if (canceled) {
+        return;
+      }
+      setWorkouts(data);
+    });
+    return () => {
+      canceled = true;
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      {workouts === null ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {workouts.results.map((workout) => (
+            <p key={workout.id}>
+              {workout.userName} - {workout.distance}m
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
