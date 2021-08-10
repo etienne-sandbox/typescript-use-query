f4a51b84763b7b3fef7413c14735d5cc26af1667
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2021-08-10 15:59:01 +0200

    Add workouts

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,43 @@ const PlaceSchema = z.object({
   workoutCount: z.number(),
 });
 
-const PlacesResponseSchema = z.object({
-  results: z.array(PlaceSchema),
-  total: z.number(),
-});
+const PlacesResponseSchema = responseSchema(PlaceSchema);
 
 type PlacesResponse = z.infer<typeof PlacesResponseSchema>;
+
+const WorkoutSchema = z.object({
+  id: z.string(),
+  date: z.string(),
+  place: z.string(),
+  distance: z.number(),
+  duration: z.number(),
+  user: z.string(),
+  placeName: z.string(),
+  speed: z.number(),
+  userName: z.string(),
+});
+
+function responseSchema<T>(schema: z.Schema<T>) {
+  return z.object({
+    results: z.array(schema),
+    total: z.number(),
+  });
+}
+
+const WorkoutResponseSchema = responseSchema(WorkoutSchema);
+
+type WorkoutResponse = z.infer<typeof WorkoutResponseSchema>;
 
 async function fetchPlaces() {
   const response = await fetch("http://localhost:3001/places");
   const data = await response.json();
   return PlacesResponseSchema.parse(data);
+}
+
+async function fetchWorkouts() {
+  const response = await fetch("http://localhost:3001/workouts");
+  const data = await response.json();
+  return WorkoutResponseSchema.parse(data);
 }
 
 function Places() {
@@ -53,13 +79,49 @@ function Places() {
   );
 }
 
+function Workouts() {
+  const [workouts, setWorkouts] = useState<WorkoutResponse | null>(null);
+
+  useEffect(() => {
+    let canceled = false;
+    fetchWorkouts().then((workouts) => {
+      if (canceled) {
+        return;
+      }
+      setWorkouts(workouts);
+    });
+    return () => {
+      canceled = true;
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      Hello React
+      {workouts === null ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {workouts.results.map((workout) => (
+            <li key={workout.id}>
+              {workout.userName} - {workout.distance / 1000}km
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
 function App() {
-  const [showPlaces, setShowPlaces] = useState(false);
+  const [page, setPage] = useState<"places" | "workouts">("places");
 
   return (
     <div>
-      <button onClick={() => setShowPlaces((p) => !p)}>Toggle</button>
-      {showPlaces && <Places />}
+      <button onClick={() => setPage("places")}>Places</button>
+      <button onClick={() => setPage("workouts")}>Workouts</button>
+      {page === "places" && <Places />}
+      {page === "workouts" && <Workouts />}
     </div>
   );
 }
