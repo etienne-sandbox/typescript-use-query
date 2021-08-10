1c0c6171c49a33a5c2f7251d9593d0f2d571fa0f
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2021-08-10 15:18:38 +0200

    Add zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
 import React, { useEffect, useState } from "react";
+import * as z from "zod";
 
-type Place = {
-  image: string;
-  name: string;
-  slug: string;
-  workoutCount: number;
-};
+const PlaceSchema = z.object({
+  image: z.string(),
+  name: z.string(),
+  slug: z.string(),
+  workoutCount: z.number(),
+});
 
-type PlacesResponse = {
-  results: Array<Place>;
-  total: number;
-};
+const PlacesResponseSchema = z.object({
+  results: z.array(PlaceSchema),
+  total: z.number(),
+});
 
-async function fetchPlaces(): Promise<PlacesResponse> {
+type PlacesResponse = z.infer<typeof PlacesResponseSchema>;
+
+async function fetchPlaces() {
   const response = await fetch("http://localhost:3001/places");
   const data = await response.json();
-  return data;
+  return PlacesResponseSchema.parse(data);
 }
 
-function App() {
+function Places() {
   const [places, setPlaces] = useState<PlacesResponse | null>(null);
 
   useEffect(() => {
+    let canceled = false;
     fetchPlaces().then((places) => {
+      if (canceled) {
+        return;
+      }
       setPlaces(places);
     });
+    return () => {
+      canceled = true;
+    };
   }, []);
 
   return (
@@ -39,6 +49,17 @@ function App() {
           ))}
         </ul>
       )}
+    </div>
+  );
+}
+
+function App() {
+  const [showPlaces, setShowPlaces] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setShowPlaces((p) => !p)}>Toggle</button>
+      {showPlaces && <Places />}
     </div>
   );
 }
