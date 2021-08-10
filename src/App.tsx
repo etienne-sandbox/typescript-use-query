3456ea3d224187049f626846e8b0bff1ffb2183d
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2021-08-10 14:48:59 +0200

    Fetch places

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Place = {
+  image: string;
+  name: string;
+  slug: string;
+  workoutCount: number;
+};
+
+type PlacesResponse = {
+  results: Array<Place>;
+  total: number;
+};
+
+async function fetchPlaces(): Promise<PlacesResponse> {
+  const response = await fetch("http://localhost:3001/places");
+  const data = await response.json();
+  return data;
+}
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [places, setPlaces] = useState<PlacesResponse | null>(null);
+
+  useEffect(() => {
+    fetchPlaces().then((places) => {
+      setPlaces(places);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      Hello React
+      {places === null ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {places.results.map((place) => (
+            <li key={place.slug}>{place.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
