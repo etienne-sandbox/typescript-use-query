b35ea01b2e8978a027876da74baef8460d60c0e5
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2021-12-07 14:18:48 +0100

    Load & display workouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Workout = {
+  id: string;
+  date: string;
+  place: string;
+  distance: number;
+  duration: number;
+  user: string;
+  placeName: string;
+  speed: number;
+  userName: string;
+};
+
+type WorkoutsResult = {
+  total: number;
+  results: Workout[];
+};
 
 function App() {
-  return <div className="App">Hello React</div>;
+  const [data, setData] = useState<null | WorkoutsResult>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/workouts")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Workouts</h1>
+      <div>
+        {data === null ? (
+          <p>Loading...</p>
+        ) : (
+          data.results.map((workout) => {
+            return (
+              <p key={workout.id}>
+                {workout.date} - {workout.distance}m - {workout.duration}
+              </p>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
